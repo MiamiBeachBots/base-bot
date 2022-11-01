e0454f5734852fb792e4ee538e7a09b290d37584
Jack Nelson <jack@jacknelson.xyz>
2022-11-01 18:18:03 -0400

    fix formatting

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,64 +4,63 @@
 
 package frc.robot.subsystems;
 
-// import motors
+// import motor & frc dependencies
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.drive.*;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.motorcontrol.Victor;
-
-// dependency import
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class DriveTrain extends SubsystemBase {
-    /** Creates a new ExampleSubsystem. */
+  /** Creates a new ExampleSubsystem. */
 
-    // math constants
-    private final double DELTA = 0.05;
-    private final double KP = 0.015;
+  // math constants
+  private final double DELTA = 0.05;
 
-    // motors
-    private Victor m_frontRight;
-    private Victor m_backLeft;
-    private Victor m_backRight;
-    private Victor m_frontLeft;
+  private final double KP = 0.015;
 
-    // motors controllers
-    private MotorControllerGroup m_left;
-    private MotorControllerGroup m_right;
+  // motors
+  private Victor m_frontRight;
+  private Victor m_backLeft;
+  private Victor m_backRight;
+  private Victor m_frontLeft;
 
-    // drive function
-    private DifferentialDrive m_ddrive;
+  // motors controllers
+  private MotorControllerGroup m_left;
+  private MotorControllerGroup m_right;
 
-    public DriveTrain() {
-        // init motors
-        m_frontRight = new Victor(Constants.MOTORFRONTRIGHTCHANNEL);
-        m_backLeft = new Victor(Constants.MOTORBACKLEFTCHANNEL);
-        m_backRight = new Victor(Constants.MOTORBACKRIGHTCHANNEL);
-        m_frontLeft = new Victor(Constants.MOTORFRONTLEFTCHANNEL);
+  // drive function
+  private DifferentialDrive m_ddrive;
 
-        // init motors controllers
-        m_left = new MotorControllerGroup(m_backLeft, m_frontLeft);
-        m_right = new MotorControllerGroup(m_frontRight, m_backRight);
+  public DriveTrain() {
+    // init motors
+    m_frontRight = new Victor(Constants.MOTORFRONTRIGHTCHANNEL);
+    m_backLeft = new Victor(Constants.MOTORBACKLEFTCHANNEL);
+    m_backRight = new Victor(Constants.MOTORBACKRIGHTCHANNEL);
+    m_frontLeft = new Victor(Constants.MOTORFRONTLEFTCHANNEL);
 
-        // init drive function
-        m_ddrive = new DifferentialDrive(m_left, m_right);
-    }
+    // init motors controllers
+    m_left = new MotorControllerGroup(m_backLeft, m_frontLeft);
+    m_right = new MotorControllerGroup(m_frontRight, m_backRight);
 
-    // default tank drive function
-    // **tank drive = specific control style where two parallel forces of motion are controlled to create linear and rotational motion
-    public void defaultDrive(double leftSpeed, double rightSpeed) {
-        m_ddrive.tankDrive(leftSpeed, rightSpeed);
-    }
+    // init drive function
+    m_ddrive = new DifferentialDrive(m_left, m_right);
+  }
 
-    @Override
-    public void periodic() {
-        // This method will be called once per scheduler run
-    }
+  // default tank drive function
+  // **tank drive = specific control style where two parallel forces of motion are controlled to
+  // create linear and rotational motion
+  public void defaultDrive(double leftSpeed, double rightSpeed) {
+    m_ddrive.tankDrive(leftSpeed, rightSpeed);
+  }
 
-    @Override
-    public void simulationPeriodic() {
-        // This method will be called once per scheduler run during simulation
-    }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
 }
