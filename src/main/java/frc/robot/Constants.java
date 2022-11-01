1fcc9d71a14c30890cd617d1ea0d4a8453871b9b
Chanathip Gulgalkhai <kenobi1337@protonmail.com>
2022-10-31 20:07:03 -0400

    Finished drivetrain code base(Using last year motors models)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,9 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final int motorFrontRightChannel = 4;
+    public static final int motorFrontLeftChannel = 1;
+    public static final int motorBackRightChannel = 3;
+    public static final int motorBackLeftChannel = 2;
+}
