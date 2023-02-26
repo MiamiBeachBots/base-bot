fb15c567ed208f2daba7aaf07af99003eb516c18
Jack Nelson <jack@jacknelson.xyz>
2023-02-26 01:02:34 -0500

    add real numbers + clean up

--- src/main/java/frc/robot/DriveConstants.java
+++ src/main/java/frc/robot/DriveConstants.java
@@ -4,10 +4,11 @@ import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
+import edu.wpi.first.math.util.Units;
 
 public final class DriveConstants {
   // general drive constants
-  public static final double WHEEL_DIAMETER = 0.1524; // meters
+  public static final double WHEEL_DIAMETER = Units.inchesToMeters(6); // meters
   public static final double PULSES_PER_REV = 2048; // resolution of encoder
   public static final double GEAR_RATIO = 1;
   public static final double DISTANCE_PER_PULSE =
@@ -18,30 +19,29 @@ public final class DriveConstants {
   // for *your* robot's drive.
   // The Robot Characterization Toolsuite provides a convenient tool for obtaining these
   // values for your robot.
-  public static final double ksVolts = 0;
-  public static final double kvVoltSecondsPerMeter = 0;
-  public static final double kaVoltSecondsSquaredPerMeter = 0;
-  public static final double kPDriveVel = 8;
+  public static final double Ks = 0.74056; // volts
+  public static final double Kv = 2.4474; // VoltSecondsPerMeter
+  public static final double Ka = 0.45481; // VoltSecondsSquaredPerMeter
+  public static final double kPDriveVel = 3.5479;
   // differental drive constants
-  public static final double kTrackwidthMeters = 0;
+  public static final double kTrackwidthMeters = Units.inchesToMeters(21.0);
   public static final DifferentialDriveKinematics kDriveKinematics =
       new DifferentialDriveKinematics(kTrackwidthMeters);
   // MAX Acceleration & Velocity
-  public static final double kMaxSpeedMetersPerSecond = 3;
-  public static final double kMaxAccelerationMetersPerSecondSquared = 1;
+  public static final double kMaxSpeed = 3; // MetersPerSecond
+  public static final double kMaxAcceleration = 1; // MetersPerSecondSquared
   // Reasonable baseline values for a RAMSETE follower in units of meters and seconds
   public static final double kRamseteB = 2;
   public static final double kRamseteZeta = 0.7;
   // Final Kinematic Configs
-  public static final SimpleMotorFeedforward FeedForward =
-      new SimpleMotorFeedforward(ksVolts, kvVoltSecondsPerMeter, kaVoltSecondsSquaredPerMeter);
+  public static final SimpleMotorFeedforward FeedForward = new SimpleMotorFeedforward(Ks, Kv, Ka);
   // Create a voltage constraint to ensure we don't accelerate too fast
   public static final DifferentialDriveVoltageConstraint autoVoltageConstraint =
       new DifferentialDriveVoltageConstraint(FeedForward, kDriveKinematics, 10);
 
   // Create config for trajectory
   public static final TrajectoryConfig driveTrajectoryConfig =
-      new TrajectoryConfig(kMaxSpeedMetersPerSecond, kMaxAccelerationMetersPerSecondSquared)
+      new TrajectoryConfig(kMaxSpeed, kMaxAcceleration)
           // Add kinematics to ensure max speed is actually obeyed
           .setKinematics(kDriveKinematics)
           // Apply the voltage constraint
