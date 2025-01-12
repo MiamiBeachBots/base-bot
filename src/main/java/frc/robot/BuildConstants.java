4c58459099133681ebf10ea4e194889489ee2240
Jack Nelson <jack@jacknelson.xyz>
2025-01-12 12:57:00 -0500

    advantagekit

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "base-bot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 37;
-  public static final String GIT_SHA = "b0332d44bdf7e27236c51f0a35b66af071f395c6";
-  public static final String GIT_DATE = "2025-01-12 12:55:35 EST";
+  public static final int GIT_REVISION = 38;
+  public static final String GIT_SHA = "7384db721e82b28fd91e4100f838f443964e1c06";
+  public static final String GIT_DATE = "2025-01-12 12:56:07 EST";
   public static final String GIT_BRANCH = "2025update";
-  public static final String BUILD_DATE = "2025-01-12 12:55:55 EST";
-  public static final long BUILD_UNIX_TIME = 1736704555006L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-01-12 12:56:45 EST";
+  public static final long BUILD_UNIX_TIME = 1736704605105L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
