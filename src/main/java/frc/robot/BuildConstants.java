b7137eac6d9b3e9ab140d637ac9a5b3245e653b9
Jack Nelson <jack@jacknelson.xyz>
2025-01-14 09:55:14 -0500

    Update BuildConstants.java

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "base-bot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 43;
-  public static final String GIT_SHA = "80b07fa69504f05661e4624a339f0b18e1159b79";
-  public static final String GIT_DATE = "2025-01-12 13:46:58 EST";
+  public static final int GIT_REVISION = 44;
+  public static final String GIT_SHA = "e7ca0413222d383700083316e0dfc28f08d81317";
+  public static final String GIT_DATE = "2025-01-14 09:54:45 EST";
   public static final String GIT_BRANCH = "2025update";
-  public static final String BUILD_DATE = "2025-01-12 13:47:11 EST";
-  public static final long BUILD_UNIX_TIME = 1736707631552L;
+  public static final String BUILD_DATE = "2025-01-14 09:55:10 EST";
+  public static final long BUILD_UNIX_TIME = 1736866510965L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
