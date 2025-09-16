79e51262a10231a43f25e34ba11d3eea988a101f
Thalia Webb <thaliathenerder@gmail.com>
2025-09-16 08:07:33 -0400

    added logging

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,7 +4,12 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Threads;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import org.littletonrobotics.junction.LogFileUtil;
@@ -14,6 +19,9 @@ import org.littletonrobotics.junction.networktables.NT4Publisher;
 import org.littletonrobotics.junction.wpilog.WPILOGReader;
 import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 import org.littletonrobotics.urcl.URCL;
+import frc.robot.Constants.Mode;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Shooter;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -23,8 +31,10 @@ import org.littletonrobotics.urcl.URCL;
  */
 public class Robot extends LoggedRobot {
   private Command m_autonomousCommand;
-
   private RobotContainer m_robotContainer;
+  private final Drivetrain m_drivetrain;
+  private final Shooter m_shooter;
+  private final XboxController m_driverController = new XboxController(0);
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -42,7 +52,7 @@ public class Robot extends LoggedRobot {
         Logger.recordMetadata("GitDirty", "All changes committed");
         break;
       case 1:
-        Logger.recordMetadata("GitDirty", "Uncomitted changes");
+        Logger.recordMetadata("GitDirty", "Uncommitted changes");
         break;
       default:
         Logger.recordMetadata("GitDirty", "Unknown");
@@ -53,15 +63,13 @@ public class Robot extends LoggedRobot {
     switch (Constants.currentMode) {
       case REAL:
         // Running on a real robot, log to a USB stick ("/U/logs")
-        Logger.addDataReceiver(new WPILOGWriter());
+        Logger.addDataReceiver(new WPILOGWriter("/U/logs"));
         Logger.addDataReceiver(new NT4Publisher());
         break;
-
       case SIM:
         // Running a physics simulator, log to NT
         Logger.addDataReceiver(new NT4Publisher());
         break;
-
       case REPLAY:
         // Replaying a log, set up replay source
         setUseTiming(false); // Run as fast as possible
@@ -73,13 +81,16 @@ public class Robot extends LoggedRobot {
 
     // Initialize URCL
     Logger.registerURCL(URCL.startExternal());
-
     // Start logging! No more data receivers, replay sources, or metadata values may be added.
     Logger.start();
 
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // Instantiate our subsystems
+    m_drivetrain = new Drivetrain();
+    m_shooter = new Shooter();
+
+    // Instantiate our RobotContainer. This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
+    m_robotContainer = new RobotContainer(m_drivetrain, m_shooter);
   }
 
   /**
@@ -97,20 +108,42 @@ public class Robot extends LoggedRobot {
     // Runs the Scheduler. This is responsible for polling buttons, adding
     // newly-scheduled commands, running already-scheduled commands, removing
     // finished or interrupted commands, and running subsystem periodic() methods.
-    // This must be called from the robot's periodic block in order for anything in
-    // the Command-based framework to work.
     CommandScheduler.getInstance().run();
 
+    // Log the current robot mode for easy analysis
+    Logger.recordOutput("Status/RobotMode", DriverStation.getRobotMode().toString());
+
+    // Log the loop cycle time for performance analysis
+    Logger.recordOutput("Status/LoopTime_ms", Timer.getFPGATimestamp() * 1000);
+
+    // Log driver station info
+    Logger.recordOutput("Status/IsAutonomous", DriverStation.isAutonomous());
+    Logger.recordOutput("Status/IsTeleop", DriverStation.isTeleop());
+    Logger.recordOutput("Status/IsDisabled", DriverStation.isDisabled());
+    Logger.recordOutput("Status/IsFMSAttached", DriverStation.isFMSAttached());
+
+    // Log controller inputs for a clear record of driver actions
+    Logger.recordOutput("DriverController/LeftX", m_driverController.getLeftX());
+    Logger.recordOutput("DriverController/LeftY", m_driverController.getLeftY());
+    Logger.recordOutput("DriverController/RightX", m_driverController.getRightX());
+    Logger.recordOutput("DriverController/RightY", m_driverController.getRightY());
+    Logger.recordOutput("DriverController/ButtonA", m_driverController.getAButton());
+    Logger.recordOutput("DriverController/ButtonB", m_driverController.getBButton());
+    Logger.recordOutput("DriverController/LeftTrigger", m_driverController.getLeftTriggerAxis());
+    
+    // Log robot pose and chassis speeds
+    Logger.recordOutput("Odometry/RobotPose", m_drivetrain.getPose());
+    Logger.recordOutput("Drivetrain/ChassisSpeeds", m_drivetrain.getChassisSpeeds());
+    
     // Return to normal thread priority
     Threads.setCurrentThreadPriority(false, 10);
-    if (m_robotContainer.enableAutoProfiling) {
-      System.out.println("WARNING, AUTO PROFILE IS ENABLED!");
-    }
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+    Logger.recordOutput("Status/RobotState", "Disabled");
+  }
 
   @Override
   public void disabledPeriodic() {}
@@ -118,6 +151,7 @@ public class Robot extends LoggedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
+    Logger.recordOutput("Status/RobotState", "Autonomous");
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     // schedule the autonomous command (example)
@@ -132,6 +166,7 @@ public class Robot extends LoggedRobot {
 
   @Override
   public void teleopInit() {
+    Logger.recordOutput("Status/RobotState", "Teleop");
     // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
@@ -147,6 +182,7 @@ public class Robot extends LoggedRobot {
 
   @Override
   public void testInit() {
+    Logger.recordOutput("Status/RobotState", "Test");
     // Cancels all running commands at the start of test mode.
     CommandScheduler.getInstance().cancelAll();
   }
@@ -157,9 +193,12 @@ public class Robot extends LoggedRobot {
 
   /** This function is called once when the robot is first started up. */
   @Override
-  public void simulationInit() {}
+  public void simulationInit() {
+    Logger.recordOutput("Status/RobotState", "Simulation");
+  }
 
   /** This function is called periodically whilst in simulation. */
   @Override
   public void simulationPeriodic() {}
 }
+
